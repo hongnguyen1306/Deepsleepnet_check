93cd4b2cb60d72fedc4e8c210c2c7572a11d7589
Rosa123 <hongnguyenarmy@gmail.com>
2023-05-14 17:53:37 +0700

    fix batch_train

--- batch_train.sh
+++ batch_train.sh
@@ -6,11 +6,14 @@ nfolds=$3
 start=$4
 end=$5
 gpu=$6
+train=$7
+val=$8
+
 
 if [[ -n "$data" ]] && [[ -n "$start" ]] && [[ -n "$end" ]] && [[ -n "$gpu" ]]; then
     for i in $(eval echo {$start..$end})
     do
-       CUDA_VISIBLE_DEVICES=$gpu python /deepsleepnet/train.py --data_dir=$data --output_dir=$output --n_folds=$nfolds --pretrain_epochs=100 --finetune_epochs=200 --fold_idx=$i --resume=False
+       CUDA_VISIBLE_DEVICES=$gpu python train.py --data_dir=$data --train_dir=$train --val_dir=$val --output_dir=$output --n_folds=$nfolds --pretrain_epochs=100 --finetune_epochs=200 --fold_idx=$i --resume=True
     done
 else
     echo "argument error"
